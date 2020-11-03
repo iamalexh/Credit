0bffb9d603d74d084ada1fc91d47455499f8c2ba
iamalexh <67149386+iamalexh@users.noreply.github.com>
2020-11-03 14:27:32 +0000

    Create Credit.c

---
+++ Credit.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+int n;
+    do
+    {
+        n = get_string("Enter the number of the credit card: ");
+    }
+    while (n < 13 || n > 16);
+    
+    if (n == 13)
+    {
+        n.14 * 2 + n.12 * 2 + n.10
+    }
+
+}
